4e419ea0e49aa76824d3e63e39e9fb46b04aa72a
Vladimir Bliznikov <v.bliznikov@gmail.com>
2017-06-19 20:30:52 +0700

    Integrate Ace Editor from CDN sources

--- src/web-editor/ClientApp/app/components/explorer/file-editor/file-editor.component.ts
+++ src/web-editor/ClientApp/app/components/explorer/file-editor/file-editor.component.ts
@@ -1,14 +1,28 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: 'file-editor',
     templateUrl: 'file-editor.component.html',
-    styleUrls: ['../toolbar.css']
+    styleUrls: ['file-editor.component.css', '../toolbar.css']
 })
 
-export class FileEditorComponent implements OnInit {
+export class FileEditorComponent implements OnInit, AfterViewInit {
+    public sourceText: string = 'some initial text';
+    @ViewChild('editor') editorElement;
+    private aceEditor: any;
+
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+
+    }
+
+    ngAfterViewInit() {
+        let editor = ace.edit('editor');
+        editor.setFontSize("1em");
+        editor.getSession().getDocument().setValue(this.sourceText);
+
+        this.aceEditor = editor;
+    }
 }
