b96ba2175d34f15ddda7fcc8a5dc2a1741b0fd56
Vladimir Bliznikov <v.bliznikov@gmail.com>
2017-06-27 20:11:54 +0700

    Add unit test project

---
+++ src/web-editor.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CollabEdit.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
