069e988ca910695178e2d5f9f1e18016ebb85a38
Vladimir Bliznikov <v.bliznikov@gmail.com>
2017-07-02 18:16:33 +0700

    Move to Filters

--- src/web-editor/Filters/ExplorerActionFilter.cs
+++ src/web-editor/Filters/ExplorerActionFilter.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace CollabEdit.Controllers
+namespace CollabEdit.Filters
 {
     public class ExplorerActionFilter : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             base.OnActionExecuting(context);
-            
+
         }
 
         public override void OnActionExecuted(ActionExecutedContext context)
