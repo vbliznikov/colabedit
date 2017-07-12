dc481c58bba691eb9c83b50c62107bb505022bc5
Vladimir Bliznikov <v.bliznikov@gmail.com>
2017-07-12 13:41:44 +0700

    Add scafolding vcs-lib project with tests

---
+++ src/vcs-lib/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VersionControl
+{
+    public class Commit<T>
+    {
+        public Commit<T> Parent {get; internal set;}
+
+        public T Value {get; internal set;}
+    }
+}
