dfda5afe10b793f4842cb77785b0d3fd7005ae42
Vladimir Bliznikov <v.bliznikov@gmail.com>
2017-07-18 15:04:06 +0700

    Add diamond case test

--- src/vcs-lib.tests/VersionControl/RepositoryBranch.InternalsTest.cs
+++ src/vcs-lib.tests/VersionControl/RepositoryBranch.InternalsTest.cs
@@ -51,6 +51,49 @@ namespace CollabEdit.VersionControl.Tests
 
             var lca = FindCommonAcestor(commit8, commit9);
             Assert.AreEqual(commit5, lca, "The LCA should be comment5!");
+
+            lca = FindCommonAcestor(commit9, commit4);
+            Assert.AreEqual(commit4, lca, "The LCA should be commit4");
+        }
+
+        [Test]
+        public void Test_FindCommonAncestor_DiamondLeftCase()
+        {
+            //           .1
+            //        2./ \.3
+            //       4./\.5
+            //         \/
+            //          6
+
+            var root = CreateCommit(0, "root");
+            var commit2 = CreateCommit(1, "commit2", root);
+            var commit3 = CreateCommit(1, "commit3", root);
+            var commit4 = CreateCommit(2, "commit4", commit2);
+            var commit5 = CreateCommit(2, "commit5", commit2);
+            var commit6 = CreateCommit(3, "commit6", commit4, commit5);
+
+            var lca = FindCommonAcestor(commit6, commit3);
+            Assert.AreEqual(root, lca, "The LCA shoould be Root");
+        }
+
+        [Test]
+        public void Test_FindCommonAncestor_DiamondRightCase()
+        {
+            //           .1
+            //        2./ \.3
+            //          4./\.5
+            //            \/
+            //             6
+
+            var root = CreateCommit(0, "root");
+            var commit2 = CreateCommit(1, "commit2", root);
+            var commit3 = CreateCommit(1, "commit3", root);
+            var commit4 = CreateCommit(2, "commit4", commit3);
+            var commit5 = CreateCommit(2, "commit5", commit3);
+            var commit6 = CreateCommit(3, "commit6", commit4, commit5);
+
+            var lca = FindCommonAcestor(commit6, commit2);
+            Assert.AreEqual(root, lca, "The LCA shoould be Root");
         }
     }
 }
