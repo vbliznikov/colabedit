a1c79bd288c88ba44a94b0fb670209dc466254c2
Vladimir Bliznikov <v.bliznikov@gmail.com>
2017-07-30 14:45:23 +0700

    Add test cases for line merge

--- src/vcs-lib.tests/VersionControl/Operations/TestStringMergeHandler.cs
+++ src/vcs-lib.tests/VersionControl/Operations/TestStringMergeHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
@@ -23,11 +24,30 @@ namespace CollabEdit.VersionControl.Operations.Tests
              "Blue fox jumps",
             "Brown fox jumps")]
         [TestCase("Fox eats the frog", "Pig grunt", "Pig grunt", "Pig grunt")]
-        public void TestCompareDiffs(string origin, string left, string right, string expected)
+        [TestCaseSource(typeof(StringMergeCases))]
+        public void TestMergeWords(string origin, string left, string right, string expected)
         {
             var mergeHandler = new StringMergeHandler();
             var mergeResult = mergeHandler.Merge(origin, left, right, ConflictResolutionOptions.RaiseException);
             Assert.That(mergeResult, Is.EqualTo(expected));
+        }
+    }
+
+    internal class StringMergeCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return LineInsertions();
+        }
+
+        public TestCaseData LineInsertions()
+        {
+            var args = new string[4];
+            args[0] = "\n\r";                                      // origin
+            args[1] = "New line above.\n\r\n\r";                   // left
+            args[2] = "\n\rNew line below.\n\r";                   // right
+            args[3] = "New line above.\n\r\n\rNew line below.\n\r";//expected
+            return new TestCaseData(args).SetName("New Line insertions left and right");
         }
     }
 }
